c3e7f727bede5706d40d2580b17c2f96ed78da77
EmySim <145769796+EmySim@users.noreply.github.com>
2025-11-06 22:12:40 +0100

    faketest

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.bobapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void faketest() {
+		Assertions.assertEquals(1, 0);
 	}
 
 }
